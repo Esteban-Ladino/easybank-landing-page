e5c2a1a99cb633967bd1e474107cd4e5dadcab27
Esteban Ladino <esteban.ladino.927@gmail.com>
2020-05-13 18:03:01 -0500

    Updates

---
+++ precache-manifest.374976b326b800330101b5e9556da373.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20b0426b5375e298b58019957f453281",
+    "url": "/easybank-landing-page/index.html"
+  },
+  {
+    "revision": "6afcf62e4a01b0c48a00",
+    "url": "/easybank-landing-page/static/css/main.13bd6499.chunk.css"
+  },
+  {
+    "revision": "3ec0e15c97a205d75f72",
+    "url": "/easybank-landing-page/static/js/2.d9e3b21d.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/easybank-landing-page/static/js/2.d9e3b21d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6afcf62e4a01b0c48a00",
+    "url": "/easybank-landing-page/static/js/main.61447aad.chunk.js"
+  },
+  {
+    "revision": "059d4b27d9fe2efe8843",
+    "url": "/easybank-landing-page/static/js/runtime-main.1eb19c22.js"
+  },
+  {
+    "revision": "9cea2f98108b16ed09dec91dc0e5a4d4",
+    "url": "/easybank-landing-page/static/media/bg-intro-desktop.9cea2f98.svg"
+  },
+  {
+    "revision": "c23dd1d0b7e4b4a1637d50cbb9fff810",
+    "url": "/easybank-landing-page/static/media/bg-intro-mobile.c23dd1d0.svg"
+  },
+  {
+    "revision": "db3d89069f8292defec0f180493a8817",
+    "url": "/easybank-landing-page/static/media/icon-api.db3d8906.svg"
+  },
+  {
+    "revision": "d4cdc9d6b3c930ddaff8dc79acc005a1",
+    "url": "/easybank-landing-page/static/media/icon-budgeting.d4cdc9d6.svg"
+  },
+  {
+    "revision": "b098a76cd2558ce72a7b730de0d57503",
+    "url": "/easybank-landing-page/static/media/icon-close.b098a76c.svg"
+  },
+  {
+    "revision": "1a81861b7aaa8c30169d65faf9027218",
+    "url": "/easybank-landing-page/static/media/icon-facebook.1a81861b.svg"
+  },
+  {
+    "revision": "72bac04e38489fa2115bb7c45775578f",
+    "url": "/easybank-landing-page/static/media/icon-hamburger.72bac04e.svg"
+  },
+  {
+    "revision": "e0814599d1e638a04578e79a652801c0",
+    "url": "/easybank-landing-page/static/media/icon-instagram.e0814599.svg"
+  },
+  {
+    "revision": "2fc9c776f2bb1b98e1d23bc813261bd3",
+    "url": "/easybank-landing-page/static/media/icon-onboarding.2fc9c776.svg"
+  },
+  {
+    "revision": "7a9111dbfa3b4b2ea0d088435bfe1d76",
+    "url": "/easybank-landing-page/static/media/icon-online.7a9111db.svg"
+  },
+  {
+    "revision": "ef15af23a9c6edf1014f6f44b31c8b78",
+    "url": "/easybank-landing-page/static/media/icon-pinterest.ef15af23.svg"
+  },
+  {
+    "revision": "064c31714c77b6580c3e4e8176c435fb",
+    "url": "/easybank-landing-page/static/media/icon-twitter.064c3171.svg"
+  },
+  {
+    "revision": "8e2f566fcefd23c8aacdaa5a83cdaff2",
+    "url": "/easybank-landing-page/static/media/icon-youtube.8e2f566f.svg"
+  },
+  {
+    "revision": "1510e4a211f7a044236decc6d9b4afe9",
+    "url": "/easybank-landing-page/static/media/image-confetti.1510e4a2.jpg"
+  },
+  {
+    "revision": "0f583e2b521e1590dd7ab8366c478227",
+    "url": "/easybank-landing-page/static/media/image-currency.0f583e2b.jpg"
+  },
+  {
+    "revision": "edc63e85927303b6c172617a5d1742b6",
+    "url": "/easybank-landing-page/static/media/image-mockups.edc63e85.png"
+  },
+  {
+    "revision": "c619f6cb0554ff2c465d2bb6268cb493",
+    "url": "/easybank-landing-page/static/media/image-plane.c619f6cb.jpg"
+  },
+  {
+    "revision": "b561f8d50067b0ce1c2d4e8b54671922",
+    "url": "/easybank-landing-page/static/media/image-restaurant.b561f8d5.jpg"
+  },
+  {
+    "revision": "fe5c6dcee699e86a25a18112801ec30f",
+    "url": "/easybank-landing-page/static/media/logoDark.fe5c6dce.svg"
+  },
+  {
+    "revision": "dfbe449be78f454c8d37dbe2ac0bca35",
+    "url": "/easybank-landing-page/static/media/logoLight.dfbe449b.svg"
+  }
+]);
